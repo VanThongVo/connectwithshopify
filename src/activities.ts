44615d856110df45a88935f8f40608a2b0d6ca4d
VanThongVo <vovanthong14082000@gmail.com>
2024-10-05 13:15:29 +0700

    init

--- src/activities.ts
+++ src/activities.ts
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 const shopifyHost = 'vtlclothingstore.myshopify.com';
 const shopifyVersion = '2023-07';
-const url = `https://${shopifyHost}/admin/api/${shopifyVersion}`;
-const headers = {
-  'Content-Type': 'application/json',
-  'X-Shopify-Access-Token': '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'
-};
+
 
 // REST APIs
 export async function getProduct(id: string): Promise<string>{
